0a23e897b6499e657e88891dd951f2c1f1c401b5
disappearwind <disappearwind@81bae8f6-6976-edd8-0f80-442a59dceae4>
2014-11-03 09:51:11 +0000

    第一次联调漏掉的文件

---
+++ trunk/Web/Models/Album.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Disappearwind.PortalSolution.PortalWeb.Models
+{
+    public partial class Album
+    {
+        public List<string> PicList { get; set; }
+    }
+}
